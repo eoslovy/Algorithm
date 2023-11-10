d47908a86a87c052e66729df8123f9f3f5fac340
윤덕건 <50583453+eoslovy@users.noreply.github.com>
2023-11-10 17:28:06 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 460 ms, Memory: 50548 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+oasis = [int(input()) for _ in range(N)]
+
+stack = []
+result = 0
+
+for i in oasis:
+    while stack and stack[-1][0] < i:
+        result+= stack.pop()[1]
+    if not stack:
+        stack.append((i,1))
+        continue
+    if stack[-1][0]==i:
+        cnt = stack.pop()[1]
+        result+= cnt
+    
+        if stack: result+=1
+        stack.append((i,cnt+1))
+    else:
+        stack.append((i,1))
+        result+=1
+
+print(result)

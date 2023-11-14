c03e096ff26f6cef0800aa89478e165562606999
윤덕건 <50583453+eoslovy@users.noreply.github.com>
2023-11-14 16:11:16 +0900

    [Silver IV] Title: 큐 2, Time: 1560 ms, Memory: 173080 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N = int(input())
+
+queue = deque([])
+
+for i in range(N):
+    order = input().split()
+    if order[0] == 'push':
+        queue.append(order[1])
+    elif order[0] == 'pop':
+        if len(queue) == 0:
+            print('-1')
+        else: print(queue.popleft())
+    elif order[0] == 'size':
+        print(len(queue))
+    elif order[0] == 'empty':
+        if len(queue) == 0:
+            print('1')
+        else:print('0')
+    elif order[0] == 'front':
+        if len(queue) == 0:
+            print('-1')
+        else:print(queue[0])
+    elif order[0] == 'back':
+        if len(queue) == 0:
+            print('-1')
+        else:print(queue[-1])

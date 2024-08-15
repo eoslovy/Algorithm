11a723e4b4e8f87f9024eef792092c44ea3fa727
윤덕건 <50583453+eoslovy@users.noreply.github.com>
2024-08-15 16:52:18 +0900

    [Silver III] Title: N과 M (1), Time: 160 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(cnt, ans):
+    if cnt == m:
+        print(*ans)
+        return
+
+    for i in range(1, n + 1):
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(cnt + 1, ans + [i])
+            visited[i] = 0
+
+
+n, m = map(int, input().split())
+lst = []
+visited = [0] * (n + 1)
+for i in range(1, n + 1):
+    lst.append(i)
+
+dfs(0, [])

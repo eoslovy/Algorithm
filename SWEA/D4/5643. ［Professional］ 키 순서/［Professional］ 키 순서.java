4b9b724e5286e0526dd002896085bf99409ca360
윤덕건 <50583453+eoslovy@users.noreply.github.com>
2024-09-05 13:14:25 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,461 ms, Memory: 92,852 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	private static int n, m;
+	private static int[][] arr;
+	private static int[][] cnt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int t = Integer.parseInt(st.nextToken());
+
+		for (int tc = 1; tc <= t; tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			m = Integer.parseInt(st.nextToken());
+			arr = new int[n + 1][n + 1];
+			cnt = new int[n + 1][2];
+
+			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[a][b] = 1;
+				arr[b][a] = -1;
+			}
+
+			for (int i = 1; i <= n; i++) {
+				int[] visited1 = new int[n + 1];
+				int[] visited2 = new int[n + 1];
+				dfs1(i, visited1);
+				dfs2(i, visited2);
+			}
+
+			int ans = 0;
+			for (int i = 1; i <= n; i++) {
+				if (cnt[i][0] + cnt[i][1] == n - 1) {
+					ans++;
+				}
+			}
+			System.out.printf("#%d %d\n", tc, ans);
+		}
+	}
+
+	private static void dfs1(int x, int[] visited) {
+		visited[x] = 1;
+		for (int i = 1; i <= n; i++) {
+			if (arr[x][i] == 1 && visited[i] == 0) {
+				cnt[i][1]++;
+				dfs1(i, visited);
+			}
+		}
+	}
+
+	private static void dfs2(int x, int[] visited) {
+		visited[x] = 1;
+		for (int i = 1; i <= n; i++) {
+			if (arr[x][i] == -1 && visited[i] == 0) {
+				cnt[i][0]++;
+				dfs2(i, visited);
+			}
+		}
+	}
+}

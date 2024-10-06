276b60955e14148641a4895bae9548418cae3403
eoslovy <50583453+eoslovy@users.noreply.github.com>
2024-10-06 14:58:12 +0900

    [Gold III] Title: 파티, Time: 172 ms, Memory: 18684 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,21 +1,25 @@
 import java.io.*;
 import java.util.*;
 
-class Edge {
-    int from, to, cost;
+class Edge implements Comparable<Edge> {
+    int to, cost;
 
-    public Edge(int from, int to, int cost) {
-        this.from = from;
+    public Edge(int to, int cost) {
         this.to = to;
         this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.cost - o.cost;
     }
 }
 
 public class Main {
     private static int n, m, x;
     private static int INF = Integer.MAX_VALUE;
-    static List<Edge> edges = new ArrayList<>();
-    static long[] dist1, dist2, result;
+    static List<Edge>[] graph, reverseGraph;
+    static long[] dist1, dist2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,46 +28,60 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
 
+
         dist1 = new long[n + 1];
         dist2 = new long[n + 1];
-        result = new long[n + 1];
+        graph = new ArrayList[n + 1];
+        reverseGraph = new ArrayList[n + 1];
         Arrays.fill(dist1, INF);
         Arrays.fill(dist2, INF);
-        dist1[x] = 0;
-        dist2[x] = 0;
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+            reverseGraph[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(from, to, cost));
+            graph[from].add(new Edge(to, cost));
+            reverseGraph[to].add(new Edge(from, cost));
         }
-        solve();
+        solve(x, dist1, graph);
+        solve(x, dist2, reverseGraph);
 
-        for (int i = 0; i < n + 1; i++) {
-            if (dist1[i] != INF && dist2[i] != INF)
-                result[i] = dist1[i] + dist2[i];
-        }
-
-        Arrays.sort(result);
-
-        for (int i = result.length - 1; i >= 0; i--) {
-            if (result[i] != INF) {
-                System.out.println(result[i]);
-                break;
+        long result = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist1[i] != INF && dist2[i] != INF) {
+                result = Math.max(result, dist1[i] + dist2[i]);
             }
         }
+
+        System.out.println(result);
     }
 
-    private static void solve() {
-        for (int i = 0; i < n; i++) {
-            for (Edge edge : edges) {
-                if (dist1[edge.from] != INF && dist1[edge.to] > dist1[edge.from] + edge.cost) {
-                    dist1[edge.to] = dist1[edge.from] + edge.cost;
-                }
-                if (dist2[edge.to] != INF && dist2[edge.from] > dist2[edge.to] + edge.cost) {
-                    dist2[edge.from] = dist2[edge.to] + edge.cost;
+    private static void solve(int start, long[] dist, List<Edge>[] graph) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
+            int now = current.to;
+            int cost = current.cost;
+
+            if (dist[now] < cost)
+                continue;
+
+            for (Edge edge : graph[now]) {
+                int next = edge.to;
+                int nextCost = cost + edge.cost;
+
+                if (dist[next] > nextCost) {
+                    dist[next] = nextCost;
+                    pq.add(new Edge(next, nextCost));
                 }
             }
         }

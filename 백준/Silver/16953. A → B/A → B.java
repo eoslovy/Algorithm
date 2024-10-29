9476af00d4e442e2f354b2999cf735cc4250ea06
eoslovy <50583453+eoslovy@users.noreply.github.com>
2024-10-29 11:47:46 +0900

    [Silver II] Title: A → B, Time: 108 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int a, b, ans;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+
+		ans = Integer.MAX_VALUE;
+		dfs(b, 0);
+		System.out.println(ans + 1);
+	}
+
+	private static void dfs(int x, int cnt) {
+		if (x == a) {
+			ans = Math.min(ans, cnt);
+			return;
+		}
+
+		if (x % 10 == 1) {
+//			System.out.println(x);
+//			System.out.println("------");
+			dfs((x - 1) / 10, cnt + 1);
+		}
+
+		else if (x > a && x % 2 == 0) {
+//			System.out.println(x);
+//			System.out.println("--aa--");
+			dfs(x / 2, cnt + 1);
+		}
+
+		else
+			ans = -2;
+	}
+
+}

d7164c15033a9bd91937a17c3c8bb0c40c849c9f
eoslovy <50583453+eoslovy@users.noreply.github.com>
2024-12-06 17:20:08 +0900

    [Gold IV] Title: 알고스팟, Time: 112 ms, Memory: 14708 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -2,72 +2,55 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	private static int n, m;
-	private static int[] dx = {0,0,1,-1};
-	private static int[] dy = {1,-1,0,0};
-	private static int[][] graph, visited;
+    private static int n, m;
+    private static int[] dx = {0, 0, 1, -1};
+    private static int[] dy = {1, -1, 0, 0};
+    private static int[][] graph, visited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		m = Integer.parseInt(st.nextToken());
-		n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         graph = new int[n][m];
         visited = new int[n][m];
 
 
-		for(int i=0; i<n; i++) {
-			String tmp = br.readLine();
-			for(int j=0; j<m; j++) {
-			    if(tmp.charAt(j)=='0')
-				    graph[i][j]=0;
-				if(tmp.charAt(j)=='1')
-				    graph[i][j]=1;
-			}
-		}
+        for (int i = 0; i < n; i++) {
+            String tmp = br.readLine();
+            for (int j = 0; j < m; j++) {
+                if (tmp.charAt(j) == '0')
+                    graph[i][j] = 0;
+                if (tmp.charAt(j) == '1')
+                    graph[i][j] = 1;
+            }
+        }
 
-		Deque<int[]> q = new LinkedList<>();
-// 		Queue<int[]> p = new LinkedList<>();
-		q.add(new int[] {0,0});
-		visited[0][0]=1;
+        Deque<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        visited[0][0] = 1;
 
-        int cnt=2;
-        
-        while(visited[n-1][m-1] == 0 && !q.isEmpty()){
-    		while(!q.isEmpty()) {
-    			int[] tmp = q.poll();
-    			int x=tmp[0];
-    			int y=tmp[1];
-    			for(int i=0; i<4; i++) {
-    				int nx = x+dx[i];
-    				int ny = y+dy[i];
-    				if(0<=nx&&nx<n&&0<=ny&&ny<m&&visited[nx][ny]==0) {
-    					if(graph[nx][ny]==0) {
-    						visited[nx][ny]=visited[x][y];
-    						q.addFirst(new int[] {nx,ny});
-    					}
-    					if(graph[nx][ny]==1) {
-    					    visited[nx][ny]=visited[x][y]+1;
-    						q.add(new int[] {nx,ny});
-    					}
-    				}
-    			}
-    		}
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+            int x = tmp[0];
+            int y = tmp[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (0 <= nx && nx < n && 0 <= ny && ny < m && visited[nx][ny] == 0) {
+                    if (graph[nx][ny] == 0) {
+                        visited[nx][ny] = visited[x][y];
+                        q.addFirst(new int[]{nx, ny});
+                    }
+                    if (graph[nx][ny] == 1) {
+                        visited[nx][ny] = visited[x][y] + 1;
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
 
-    // 		while(!p.isEmpty()) {
-    // 			int[] tmp = p.poll();
-    // 			int x=tmp[0];
-    // 			int y=tmp[1];
-    // 			visited[x][y]=cnt;
-    // 			q.add(new int[]{x,y});
-    // 		}
-    		cnt++;
-    	}
-    	
-		if(visited[n-1][m-1]!=0)
-		    System.out.println(visited[n-1][m-1]-1);
-		else
-		    System.out.println(-1);
+        System.out.println(visited[n - 1][m - 1] - 1);
     }
 }

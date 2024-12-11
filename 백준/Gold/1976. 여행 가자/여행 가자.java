39fb15c0d9878ae71c5e34a8edf22851d51adabc
eoslovy <50583453+eoslovy@users.noreply.github.com>
2024-12-12 00:09:59 +0900

    [Gold IV] Title: 여행 가자, Time: 164 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 public class Main
 {
+    private static int[] parent;
+    
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,50 +13,57 @@ public class Main
 		st = new StringTokenizer(br.readLine());
 		int m=Integer.parseInt(st.nextToken());
 		
-		int[][] graph=new int[n][n];
+		parent=new int[n];
+		
+		for(int i=0;i<n;i++){
+		    parent[i]=i;
+		}
+		
 		for(int i=0;i<n;i++){
 		    st = new StringTokenizer(br.readLine());
 		    for(int j=0;j<n;j++){
-		        graph[i][j]=Integer.parseInt(st.nextToken());
+		        int connected=Integer.parseInt(st.nextToken());
+		        if(connected==1)
+		            union(i,j);
 		    }
 		}
 		
 		int[] plan=new int[m];
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<m;i++){
-		    plan[i]=Integer.parseInt(st.nextToken());
+		    plan[i]=Integer.parseInt(st.nextToken())-1;
 		}
 		
-		for(int i=0;i<n;i++){
-		    graph[i][i]=1;
-		}
-		
-		for(int k=0;k<n;k++){
-		    for(int i=0;i<n;i++){
-		        for(int j=0;j<n;j++){
-		            if(graph[i][k]==1&&graph[k][j]==1){
-		                graph[i][j]=1;
-		            }
-		        }
-		    }
-		}
+
 		
 		int flag=0;
-		for(int i=0;i<m-1;i++){
-		    if(graph[plan[i]-1][plan[i+1]-1]==0){
+		int root=find(plan[0]);
+		for(int i=1;i<m;i++){
+		    if(find(plan[i])!=root){
 		        flag=1;
 		        break;
 		    }
 		}
 		
-// 		for(int i=0;i<n;i++){
-// 		    System.out.println(Arrays.toString(graph[i]));
-// 		}
-		
 		if(flag==0)
 		    System.out.println("YES");
 		else
 		    System.out.println("NO");
-
+        
 	}
+
+    static int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+	
+	static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
+        }
+    }
 }

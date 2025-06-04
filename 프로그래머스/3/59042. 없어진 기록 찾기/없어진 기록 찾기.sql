adc05c1e7bd2cf95c454e3e2837eee42ab452657
DeokGeon Yun <50583453+eoslovy@users.noreply.github.com>
2025-06-04 14:33:59 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,4 +1,4 @@
-select animal_id, name
-from animal_outs
-where animal_id not in (select animal_id from animal_ins)
-order by animal_id
+SELECT A.ANIMAL_ID, A.NAME
+FROM ANIMAL_OUTS A
+WHERE A.ANIMAL_ID NOT IN(SELECT ANIMAL_ID FROM ANIMAL_INS)
+ORDER BY A.ANIMAL_ID, A.NAME
